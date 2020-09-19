68aa21edd97abbcfbff11f93fc8ad0ba605db5e9
Kamryn Ohly <kamohly@gmail.com>
2020-09-19 13:30:38 -0400

    implementing NYT API

--- weGLOTogether/Controllers/OneselfViewController.swift
+++ weGLOTogether/Controllers/OneselfViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import SwiftyJSON
+import Alamofire
 
 class OneselfViewController: UIViewController {
 
+    let API_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+    let API_KEY = "0b2Bis6auaAsXPZAxBOmx2U6t8GgS4zU"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let params : [String : Any] = ["query": "covid", "api-key": API_KEY]
+        getNYTAPIData(url: API_URL, parameters: params)
         // Do any additional setup after loading the view.
     }
     
+    func getNYTAPIData(url: String, parameters: [String : Any]) {
 
+               
+                      Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+                          
+                          response in
+                          if response.result.isSuccess {
+                              print("Success! Got the API data.")
+                              
+                              let apiJSON : JSON = JSON(response.result.value!)
+                              print(apiJSON)
+//                              self.interpretYelpData(json: apiJSON)
+                          }
+                          else {
+                              print("Error \(response.result.error)")
+                          }
+                          
+                      }
+                      
+                  }
     /*
     // MARK: - Navigation
 
