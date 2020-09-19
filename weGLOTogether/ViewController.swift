50060b763cbe0fd8b6373f9afdf3f5c95dcc23d5
Kamryn Ohly <kamohly@gmail.com>
2020-09-18 23:02:22 -0400

    Initial Commit

---
+++ weGLOTogether/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weGLOTogether
+//
+//  Created by Kamryn Ohly on 9/18/20.
+//  Copyright © 2020 Kamryn Ohly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ weGLOTogetherTests/weGLOTogetherTests.swift
@@ -0,0 +1,34 @@
+//
+//  weGLOTogetherTests.swift
+//  weGLOTogetherTests
+//
+//  Created by Kamryn Ohly on 9/18/20.
+//  Copyright © 2020 Kamryn Ohly. All rights reserved.
+//
+
+import XCTest
+@testable import weGLOTogether
+
+class weGLOTogetherTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
