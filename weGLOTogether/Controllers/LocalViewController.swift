e2fef6da625393b837abe44e6c7030c20f9223cc
Kamryn Ohly <kamohly@gmail.com>
2020-09-19 17:14:31 -0400

    Yelp API successfully implemented - yessssss!

--- weGLOTogether/Controllers/LocalViewController.swift
+++ weGLOTogether/Controllers/LocalViewController.swift
@@ -12,7 +12,9 @@ import Alamofire
 
 class LocalViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    var businessArray : [CustomBusinessCell] = []
+    @IBOutlet weak var businessTypeTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    var businessArray : [BusinessData] = []
     
     @IBOutlet weak var businessTableView: UITableView!
     
@@ -24,21 +26,59 @@ class LocalViewController: UIViewController, UITableViewDelegate, UITableViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        businessTableView.register(UINib(nibName: "CustomBusinessCell", bundle: nil), forCellReuseIdentifier: "CustomBusinessCell")
+       businessTableView.register(UINib(nibName: "BusinessCell", bundle: nil), forCellReuseIdentifier: "customBusinessCell")
         let params : [String : Any] = ["term": "local restaurants", "location": "Sandusky, OH"]
-                       
+        
+        businessTableView.delegate = self
+        businessTableView.dataSource = self
+        
         getYelpAPIData(url: API_URL, parameters : params)
-
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func locationChangedPressed(_ sender: UIButton) {
+        businessArray = []
+        if let location = locationTextField.text {
+            if let type = businessTypeTextField.text {
+                let params : [String : Any] = ["term": "local \(type)", "location": location]
+                getYelpAPIData(url: API_URL, parameters: params)
+            }
+            else {
+                //do nothing
+            }
+        }
+        else {
+            //do nothing
+        }
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let url = URL(string: businessArray[indexPath.row].website) else { return }
+        UIApplication.shared.open(url)
+
+    }
+    
+   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return 200
+       }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 3
+    return businessArray.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = businessTableView.dequeueReusableCell(withIdentifier: "CustomBusinessCell", for: indexPath) as! CustomBusinessCell
+      let cell = tableView.dequeueReusableCell(withIdentifier: "customBusinessCell", for: indexPath) as! CustomBusinessCell
+    
+    
+    cell.businessImage.load(url: businessArray[indexPath.row].imageAddress)
+    print(businessArray[indexPath.row].imageAddress)
+    cell.businessTitle.text = businessArray[indexPath.row].title
+    cell.businessPrice.text = businessArray[indexPath.row].price
+    cell.businessNumber.text = businessArray[indexPath.row].phone
+    cell.businessAddress.text = businessArray[indexPath.row].address
+
     
     return cell
   }
@@ -54,6 +94,7 @@ class LocalViewController: UIViewController, UITableViewDelegate, UITableViewDat
                            
                            let apiJSON : JSON = JSON(response.result.value!)
                            print(apiJSON)
+                        
                            self.interpretYelpData(json: apiJSON)
                        }
                        else {
@@ -65,12 +106,33 @@ class LocalViewController: UIViewController, UITableViewDelegate, UITableViewDat
                }
     
     func interpretYelpData(json: JSON) {
-//        print(json)
-        var businessInfo = CustomBusinessCell()
-        var image = json["businesses"][2]["image_url"].stringValue
-        print(image)
-//        businessInfo.businessImage = UIImageView.load(url: URL(image))
-        print("TESTING")
+        print(json.count)
+        var i = 0
+        while (i < 6)  {
+            print(json["businesses"][i]["name"])
+            if let indexWorks = Optional(json["businesses"][i]["name"]) {
+                let businessInfo = BusinessData()
+                if let img = json["businesses"][i]["image_url"].url {
+                    businessInfo.imageAddress = img
+                } else {
+                    //no image
+                }
+
+                 businessInfo.title = json["businesses"][i]["name"].stringValue
+                 businessInfo.phone = json["businesses"][i]["display_phone"].stringValue
+                 businessInfo.price = json["businesses"][i]["price"].stringValue
+                 businessInfo.address = json["businesses"][i]["location"]["display_address"][0].stringValue + ", " + json["businesses"][i]["location"]["display_address"][1].stringValue
+                 businessInfo.website = json["businesses"][i]["url"].stringValue
+                businessArray.append(businessInfo)
+            } else {
+                //do nothing
+            }
+            
+            i += 1
+        }
+        print("successfully appended business data points")
+
+        businessTableView.reloadData()
     }
     /*
     // MARK: - Navigation
@@ -83,16 +145,17 @@ class LocalViewController: UIViewController, UITableViewDelegate, UITableViewDat
     */
 
 }
-//extension UIImageView {
-//    func load(url: URL) {
-//        DispatchQueue.global().async { [weak self] in
-//            if let data = try? Data(contentsOf: url) {
-//                if let image = UIImage(data: data) {
-//                    DispatchQueue.main.async {
-//                        self?.image = image
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ weGLOTogether/Model/BusinessData.swift
@@ -0,0 +1,20 @@
+//
+//  BusinessData.swift
+//  weGLOTogether
+//
+//  Created by Kamryn Ohly on 9/19/20.
+//  Copyright © 2020 Kamryn Ohly. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BusinessData {
+    
+    var title = ""
+    var imageAddress = URL(fileURLWithPath: "")
+    var price = ""
+    var address = ""
+    var phone = ""
+    var website = ""
+}
