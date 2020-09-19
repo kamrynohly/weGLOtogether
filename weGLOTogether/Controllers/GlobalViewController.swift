5d3c7193d186ede4e8a1380e6a23c72863562361
Kamryn Ohly <kamohly@gmail.com>
2020-09-19 03:51:44 -0400

    Added generic view controllers & linked code files

---
+++ weGLOTogether/Controllers/GlobalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GlobalViewController.swift
+//  weGLOTogether
+//
+//  Created by Kamryn Ohly on 9/19/20.
+//  Copyright © 2020 Kamryn Ohly. All rights reserved.
+//
+
+import UIKit
+
+class GlobalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
